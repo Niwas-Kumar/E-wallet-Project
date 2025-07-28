52c15f17a8b11007f3e1ca5966aa0e65631625f8
Niwas Kumar <niwastechster@gmail.com>
2025-07-28 23:56:07 +0530

    endpoints changing second

--- ewallet-api/src/main/java/com/ewallet/ewallet_api/service/CustomUserDetailsService.java
+++ ewallet-api/src/main/java/com/ewallet/ewallet_api/service/CustomUserDetailsService.java
@@ -1,7 +1,8 @@
 package com.ewallet.ewallet_api.service;
 
+import com.ewallet.ewallet_api.entity.User;
 import com.ewallet.ewallet_api.repository.UserRepository;
-//import com.ewallet.ewallet_api.entity.User;
+import com.ewallet.ewallet_api.security.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,7 +17,9 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return (UserDetails) userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new CustomUserDetails(user);
     }
 }
