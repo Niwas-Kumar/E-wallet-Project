a420e22d331983d8f86290ccc04f670e3da6b32a
Niwas Kumar <niwastechster@gmail.com>
2025-07-28 23:57:24 +0530

    endpoints changing

--- ewallet-api/src/main/java/com/ewallet/ewallet_api/service/CustomUserDetailsService.java
+++ ewallet-api/src/main/java/com/ewallet/ewallet_api/service/CustomUserDetailsService.java
@@ -20,6 +20,6 @@ public class CustomUserDetailsService implements UserDetailsService {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
 
-        return new CustomUserDetails(user);
+        return new CustomUserDetails(users);
     }
 }
